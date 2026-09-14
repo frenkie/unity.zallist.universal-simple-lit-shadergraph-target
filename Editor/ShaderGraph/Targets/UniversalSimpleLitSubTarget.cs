79f7c05382a144acbeb1ff240ed350f352fec88c
frenkie <frank@frankbosma.nl>
2026-09-14 13:32:13 +0200

    Shaderfix

--- Editor/ShaderGraph/Targets/UniversalSimpleLitSubTarget.cs
+++ Editor/ShaderGraph/Targets/UniversalSimpleLitSubTarget.cs
@@ -872,7 +872,9 @@ namespace UnityEditor.Rendering.Universal.ShaderGraph
                 { CoreKeywordDescriptors.LightLayers },
                 { CoreKeywordDescriptors.DebugDisplay },
                 { CoreKeywordDescriptors.LightCookies },
-#if UNITY_2022_2_OR_NEWER
+#if UNITY_6000_0_OR_NEWER
+                { CoreKeywordDescriptors.ClusterLightLoop },
+#elif UNITY_2022_2_OR_NEWER
                 { CoreKeywordDescriptors.ForwardPlus },
 #else
                 { CoreKeywordDescriptors.ClusteredRendering },
